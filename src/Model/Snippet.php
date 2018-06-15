6f85db8fffd6d1e936de568674238d1ee5aee3bb
Steve Boyd <emteknetnz@gmail.com>
2018-06-15 15:01:42 +1200

    Move ParamExpander functions

--- src/Model/Snippet.php
+++ src/Model/Snippet.php
@@ -6,8 +6,6 @@
 class Snippet extends DataObject
 {
 
-    use ParamExpander;
-
     private static $db = [
         "SnippetClass" => "Varchar(255)",
         "SnippetParams" => "Text",
@@ -75,6 +73,51 @@ class Snippet extends DataObject
         if ($provider = $this->getSnippetProvider()) {
             $params = (array)json_decode($this->SnippetParams, true);
             return $provider->getSnippets($params);
+        }
+    }
+
+
+    // ParamExpander functions
+
+    protected function expandParams($modelField, $paramFields, FieldList $fields, $tabName)
+    {
+        $fields->removeByName($modelField);
+        if ($paramFields) {
+            foreach ($paramFields as $field) {
+                $name = "COMPOUND_{$modelField}_" . $field->getName();
+                $field->setName($name);
+                $field->setValue($this->getField($name));
+                if ($tabName) {
+                    $fields->addFieldToTab($tabName, $field);
+                } else {
+                    $fields->push($field);
+                }
+            }
+        }
+    }
+
+    function getField($key) {
+        // Compound field handler
+        if(substr($key,0,9)=='COMPOUND_') {
+            list($dummy, $field, $subfield) = explode('_', $key, 3);
+            $json = json_decode(parent::getField($field), true);
+            if(isset($json[$subfield])) return $json[$subfield];
+
+        } else {
+            return parent::getField($key);
+        }
+    }
+
+    function setField($key, $val) {
+        // Compound field handler
+        if(substr($key,0,9)=='COMPOUND_') {
+            list($dummy, $field, $subfield) = explode('_', $key, 3);
+            $json = json_decode(parent::getField($field), true);
+            $json[$subfield] = $val;
+            return parent::setField($field, json_encode($json));
+
+        } else {
+            return parent::setField($key, $val);
         }
     }
 }
